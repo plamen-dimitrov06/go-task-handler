5e46d2c4c8afb101d6e7a347abe24bd9063b4838
Plamen Dimitrov <plamen.dimitrov06@abv.bg>
2024-12-10 17:33:21 +0200

    add sorting algorithm test, udpate dependencies, rename fixtures dir to testdata (unused)

---
+++ TaskProcessor/TaskSorter_test.go
@@ -0,0 +1,51 @@
+package TaskProcessor
+
+import (
+	"github.com/stretchr/testify/assert"
+	"task-handler/Models"
+	"testing"
+)
+
+func TestSortExampleTasksCorrectly(t *testing.T) {
+	tasks := []Models.Task {
+		{Name: "task-1", Command: "touch /tmp/file1"},
+		{Name: "task-2", Command: "cat /tmp/file1",  Requires: []string {"task-3"}},
+		{Name: "task-3", Command: "echo 'Hello World' > /tmp/file1",  Requires: []string {"task-1"}},
+		{Name: "task-4", Command: "rm /tmp/file1",  Requires: []string {"task-2", "task-3"}},
+
+	}
+	// slices are passed by reference (?), 
+	// so we derefence them before passing to the sorting handler
+	sut := TaskSorter{Tasks: append([]Models.Task(nil), tasks...)}
+	sortedTasks := sut.Sort()
+	tasks[1], tasks[2] = tasks[2], tasks[1]
+
+	assert.Equal(t, tasks, sortedTasks);
+}
+
+func TestSortCorreclyWhenRequiredIsMissing(t *testing.T) {
+	tasks := []Models.Task {
+		{Name: "task-1", Command: "touch /tmp/file1"},
+		{Name: "task-2", Command: "cat /tmp/file1"},
+	}
+
+	sut := TaskSorter{Tasks: append([]Models.Task(nil), tasks...)}
+	sortedTasks := sut.Sort()
+
+	assert.Equal(t, tasks, sortedTasks);
+}
+
+func TestComplexDependenciesWithIndependentTasks(t *testing.T) {
+    tasks := []Models.Task{
+        {Name: "task-1", Command: "echo 'Task 1'"},
+        {Name: "task-2", Command: "echo 'Task 2'", Requires: []string{"task-3"}},
+        {Name: "task-3", Command: "echo 'Task 3'"},
+        {Name: "task-4", Command: "echo 'Task 4'", Requires: []string{"task-2", "task-3"}},
+        {Name: "task-5", Command: "echo 'Task 5'"},
+    }
+
+    sut := TaskSorter{Tasks: append([]Models.Task(nil), tasks...)}
+    sortedTasks := sut.Sort()
+	tasks[1], tasks[2] = tasks[2], tasks[1]
+	assert.Equal(t, tasks, sortedTasks);
+}
