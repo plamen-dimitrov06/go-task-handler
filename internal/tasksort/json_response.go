c5f162bb735bcb0a2e9dd60596f22f03c9dd7a92
Plamen Dimitrov <plamen.dimitrov06@abv.bg>
2024-12-20 23:10:39 +0200

    add doc block for the json response struct

--- internal/tasksort/json_response.go
+++ internal/tasksort/json_response.go
@@ -1,5 +1,8 @@
 package tasksort
 
+/*
+ * This struct is used for specifying the fields for the JSON response.
+ */
 type JSONResponse struct {
 	Name    string `json:"name"`
 	Command string `json:"command"`
