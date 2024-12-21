e52615801dedf3620833c4d2d2c3e85e6426941c
Plamen Dimitrov <plamen.dimitrov06@abv.bg>
2024-12-21 21:48:54 +0200

    rename refactor test property from result to expected

--- internal/tasksort/sort_test.go
+++ internal/tasksort/sort_test.go
@@ -8,7 +8,7 @@ import (
 func TestSortExampleTasksCorrectly(t *testing.T) {
 	tests := map[string]struct {
 		input  []Task
-		result []Task
+		expected []Task
 	}{
 		"sort the given example tasks correctly": {
 			input: []Task{
@@ -17,7 +17,7 @@ func TestSortExampleTasksCorrectly(t *testing.T) {
 				{Name: "task-3", Command: "echo 'Hello World' > /tmp/file1", Requires: []string{"task-1"}},
 				{Name: "task-4", Command: "rm /tmp/file1", Requires: []string{"task-2", "task-3"}},
 			},
-			result: []Task{
+			expected: []Task{
 				{Name: "task-1", Command: "touch /tmp/file1"},
 				{Name: "task-3", Command: "echo 'Hello World' > /tmp/file1", Requires: []string{"task-1"}},
 				{Name: "task-2", Command: "cat /tmp/file1", Requires: []string{"task-3"}},
@@ -29,7 +29,7 @@ func TestSortExampleTasksCorrectly(t *testing.T) {
 				{Name: "task-1", Command: "touch /tmp/file1"},
 				{Name: "task-2", Command: "cat /tmp/file1"},
 			},
-			result: []Task{
+			expected: []Task{
 				{Name: "task-1", Command: "touch /tmp/file1"},
 				{Name: "task-2", Command: "cat /tmp/file1"},
 			},
@@ -42,7 +42,7 @@ func TestSortExampleTasksCorrectly(t *testing.T) {
 				{Name: "task-4", Command: "echo 'Task 4'", Requires: []string{"task-2", "task-3"}},
 				{Name: "task-5", Command: "echo 'Task 5'"},
 			},
-			result: []Task{
+			expected: []Task{
 				{Name: "task-1", Command: "echo 'Task 1'"},
 				{Name: "task-3", Command: "echo 'Task 3'"},
 				{Name: "task-2", Command: "echo 'Task 2'", Requires: []string{"task-3"}},
@@ -55,9 +55,9 @@ func TestSortExampleTasksCorrectly(t *testing.T) {
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
-			sortedTasks := Sort(test.input)
+			actual := Sort(test.input)
 
-			assert.Equal(t, test.result, sortedTasks)
+			assert.Equal(t, test.expected, actual)
 		})
 	}
 }
